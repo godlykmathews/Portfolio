29214dd3a43faf6c5b8a8500384c2fffe4d570b7
Godly K Mathews <godlykmathews@outlook.com>
2025-01-28 21:33:00 +0530

    fix img

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
         <div className="text-center">
           <div className="mb-8">
             <img
-              src="src\images\profile.jpg"
+              src="../../images/profile.jpg"
               alt="Godly K Mathews"
               className="w-48 h-48 rounded-full mx-auto object-cover border-4 border-[#00b894]"
             />
